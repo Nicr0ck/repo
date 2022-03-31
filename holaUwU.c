86703b57141413c2d757b7a741824d16948551f1
Nicr0ck <102254176+Nicr0ck@users.noreply.github.com>
2022-03-31 12:15:01 -0300

    Add files via upload

---
+++ holaUwU.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+                printf("hola\n");
+  return 0;
+}
